a5d7cc83be6757f75a64511aefcaab4f19e7cd22
hp1heloisa <heloisapimentel2202@gmail.com>
2023-04-23 15:30:28 -0300

    versao com data teste tela 1 e 3

--- script.js
+++ script.js
@@ -51,10 +51,8 @@ function renderQuizzes(list){
                 }
                 );
                 list.data.forEach(element => {
-                all.innerHTML += `<div class="caseQuizz" onclick="playQuizz(this)" data-test="others-quiz"> 
-                                    <div class="imgCase" style="background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${element.image}); background-position: center; background-size:100%;">
+                all.innerHTML += `<div class="imgCase caseQuizz" onclick="playQuizz(this)" data-test="others-quiz" style="background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${element.image}); background-position: center; background-size:100%;">
                                         <span>${element.title}</span><span class="hidden idImagem">${element.id}</span>
-                                    </div>
                                 </div>`
                 });
             }
